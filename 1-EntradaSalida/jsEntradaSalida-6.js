7864c2ac5a93029ce1c08bec7fe0dde71ca762fe
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-07 20:57:12 -0300

    Add files via upload
    hasta ejercicio 10 hecho

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,37 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{ //1) declaro variables//
+
+    var numero1;
+
+    var numero2;
+
+    var resultado;
+
+    // 2) de dónde tomo los datos
+
+    numero1 = parseInt ( document.getElementById("numeroUno").value);
+
+    numero2 = parseInt ( document.getElementById("numeroDos").value);
+
+    resultado = numero1 + numero2;
+
+    // 3) Parseint lo que hace es transformar el texto de la variable en document a numero para permitir la suma y 
+    //    solo permite sumar números enteros
+    
+
+    alert ( " La suma es : " + resultado);
+
+    //aclaracion de parseInt
+    // STRING + STRING = STRING
+    // STRING + NUMBER = STRING
+    // NUMBER + STRING = STRING
+    // NUMBER + NUMBER = NUMBER
+    
+
+
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,70 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var num1;
+    
+    var num2;
+
+    var resultado;
+
+    num1 = parseInt (document.getElementById("numeroUno").value);
+
+    num2 = parseInt (document.getElementById("numeroDos").value);
+
+    resultado = num1 + num2;
+
+    alert("La suma es : " + resultado);
 }
 
 function restar()
 {
+    var num1;
+    
+    var num2;
+
+    var resultado;
+
+    num1 = parseInt (document.getElementById("numeroUno").value);
+
+    num2 = parseInt (document.getElementById("numeroDos").value);
+
+    resultado = num1 - num2;
+
+    alert("La resta es : " + resultado);
 	
 }
 
 function multiplicar()
 { 
-	
+	var num1;
+    
+    var num2;
+
+    var resultado;
+
+    num1 = parseInt (document.getElementById("numeroUno").value);
+
+    num2 = parseInt (document.getElementById("numeroDos").value);
+
+    resultado = num1 * num2;
+
+    alert("La multiplicación es : " + resultado);
 }
 
 function dividir()
 {
-	
+	var num1;
+    
+    var num2;
+
+    var resultado;
+
+    num1 = parseInt (document.getElementById("numeroUno").value);
+
+    num2 = parseInt (document.getElementById("numeroDos").value);
+
+    resultado = num1 / num2;
+
+    alert("La división es : " + resultado);
 }
 
