877d48269ae4699d8d89e2ef330309c2a8192b4f
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-14 22:18:08 -0300

    Add files via upload
    all

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,30 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById("mes").value;
 
-alert (mesDelAño);
+switch (mes) {
+    case "Enero":
+        case "Febrero":
+            case "Marzo":
+                case "Abril":
+                    case "Mayo":
+                        case "Junio":
+        alert("Falta para el Inivierno");
+        break;
+
+        case "Julio":
+            case "Agosto":        
+            alert("Abrigate que hace frío");
+            break;
+
+            case "Septiembre":
+                case "Octubre":
+                    case "Noviembre":
+                        case "Diciembre":
+                alert("Ya pasó el Invierno, ahora calorrr");
+                break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,7 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById("mes").value;
+
+switch (mes) {
+
+    case "Febrero":
+        alert("Este mes no tiene mas de 29 días");
+        break;
+                  case "Enero":
+                      case "Marzo":
+                          case "Abril":
+                              case "Mayo":
+                                  case "Junio":
+                                      case "Julio":
+                                          case "Agosto":
+                                              case "Septiembre":
+                                                  case "Octubre":
+                                                      case "Noviembre":
+                                                          case "Diciembre":
+                      alert("Este mes tiene 30 días o más");
+                      break;
+}
 
 //alert (mesDelAño);
 	
