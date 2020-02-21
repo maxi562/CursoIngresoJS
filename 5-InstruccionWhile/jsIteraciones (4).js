7ffc756b2b86103d8dded512eb6287697b7ec553
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-20 21:34:52 -0300

    Add files via upload
    dsfsdfd

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,25 @@
-function mostrar()
+function mostrar()  //1ª manera
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	//var numero = prompt("ingrese un número entre 0 y 10.");
 
+	//while (numero <0  || numero >9 || isNaN(numero) ) {
 
-}//FIN DE LA FUNCIÓN
+	//	numero = prompt("Error. Reingrese un número entre 0 y 10.");
+
+	//}	
+	// document.getElementById("Numero").value = numero;
+
+//}//FIN DE LA FUNCIÓN
+
+//2ª manera
+
+  	var numero = prompt("ingrese un número entre 0 y 10.");
+
+	while (!(numero >=0 && numero <=9 ) || isNaN(numero)) {
+
+		numero = prompt("Error. Reingrese un número entre 0 y 10.");
+
+	 }	
+	document.getElementById("Numero").value = numero;
+ }	
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -4,6 +4,10 @@ function mostrar()
 var sexo = prompt("ingrese f ó m .");
 
 
+while (!(sexo == "f" || sexo == "m")) {
+
+    sexo = prompt("Por favor ingrese f ó m ");
+}
 
 document.getElementById('Sexo').value=sexo;
 
