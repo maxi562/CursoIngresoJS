30f203c4227ac625501a66b35de3b99469687a2e
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-20 22:04:21 -0300

    Add files via upload
    hecho hasta el 6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,15 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero
 
+while(contador < 5) {
+	numero = parseInt(prompt("Ingrese un numero"));
+	acumulador = acumulador + numero;
+	contador++;
+}
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById("suma").value=acumulador;
+document.getElementById("promedio").value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
