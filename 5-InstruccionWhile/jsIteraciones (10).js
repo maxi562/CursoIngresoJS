08b8fb7cb9fae73356717cb1530ae7320cb675ed
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-25 20:53:24 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,12 +1,12 @@
 function mostrar()
 {
 	//declarar contadores y variables 
-	var sumapositivos;
-	var sumanegativos;
-	var contadorpositivos;
-	var contadornegativos;
-	var contadorceros;
-	var contadorpares;
+	var sumapositivos = 0;
+	var sumanegativos = 0;
+	var contadorpositivos = 0;
+	var contadornegativos = 0;
+	var contadorceros = 0;
+	var contadorpares = 0;
 	var promediopos;
 	var promedioneg;
 	var diferencia;
@@ -18,17 +18,42 @@ function mostrar()
 		while(isNaN (numero	)) {
 
 			numero = parseInt(prompt("Eso no es un numero. Ingrese un numero"));
-			
+					}		
+		if(numero >= 0) {
+			sumapositivos = numero + sumapositivos;
+			contadorpositivos++;
+		}
+		else if (numero < 0){
+			sumanegativos = numero + sumanegativos;
+			contadornegativos++;
+		}
+		else if (numero == 0) {
+			contadorceros++;
+		}
+		else if (numero % 2 == 0) {
+			contadorpares++;
 		}
 		seguir = prompt("Desea seguir ingresando numeros?");
-		if(numero > 0) {
-			sumapositivos = numero + sumapositivos;
-		}
-		if (numero < 0){
-			suman
-		}
 
 	} while (seguir == "s");
+
+	if ( contadorpositivos > 0){
+		promediopos = sumapositivos/contadorpositivos;
+	}
+	if ( contadornegativos > 0){
+		promedioneg = sumanegativos/contadornegativos;
+	}
+	diferencia = contadorpositivos - contadornegativos;
+
+	document.write("La suma de los negativos es :" + sumanegativos +"</br>");
+	document.write("La suma de los positivos es :" + sumapositivos +"</br>");
+	document.write("La cantidad de negativos es :" + contadornegativos +"</br>");
+	document.write("La cantidad de positivos es :" + contadorpositivos +"</br>");
+	document.write("La cantidad de ceros es :" + contadorceros +"</br>");
+	document.write("La cantidad de pares es :" + contadorpares +"</br>");
+	document.write("El promedio de los positivos es :" + promediopos +"</br>");
+	document.write("El promedio de los negativos es :" + promedioneg +"</br>");
+	document.write("La diferencia entre positivos y negativos es :" + diferencia +"</br>");
 
 	
 	
