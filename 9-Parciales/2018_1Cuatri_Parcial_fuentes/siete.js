05207c37d32c7cf45fb4d8071393e8a57254ee03
maxi562 <60760313+maxi562@users.noreply.github.com>
2020-02-27 22:17:00 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+    var notas;
+    var sexo;
+    var promedio;
+    var contador = 0;
+    var acumulador=0;
+    var flag = 0;
+    var contadorm;
 
-}
+    for(var x=0; x< 5; contador++){
+        notas = parseInt(prompt("Ingrese la nota del 1 al 10"));
+        sexo = prompt("Ingrese el sexo f o m");
+        acumulador = acumulador + notas;
+
+        }
+        if (flag == 0 || notas > max) {
+			max = notas;
+		}
+		if (flag == 0 || notas < min) {
+			min = notas;
+			flag = 1;			
+        }
+        if(notas = "m"){
+            contadorm++;
+        }
+
+    }
+
+
